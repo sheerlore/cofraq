829beef7ec821703b719388a3c78aa7cdc82670b
uai <forgivemyblue13@gmail.com>
2023-11-21 19:15:41 +0900

    project setup vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    \[
+      \text{Hello World} \rightarrow \text{●}
+    \]
+  </h1>
+</template>
+
+<style scoped>
+</style>
