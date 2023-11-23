7464289b390ce157b993e91b8cf7f96a48afe8ee
uai <forgivemyblue13@gmail.com>
2023-11-23 17:54:39 +0900

    pattern imple OK yattane

--- src/core/core.ts
+++ src/core/core.ts
@@ -3,7 +3,7 @@ type DisplayText = string;
 type Char = string;
 type Sequence = number[];
 type AdjMat = number[][];
-type PatternArray = { [key: number]: number[][] };
+type Patterns = { [key: number]: number[][] };
 
 // ================================================
 // Display
@@ -83,6 +83,7 @@ export function countArrow(seq: Sequence): number {
   return seq.reduce((cur, ele) => cur + ele, 0);
 }
 
+// 対応する隣接行列を作成する
 export function makeAdjMat(seq: Sequence): AdjMat | undefined {
   if (seq.length === 0) return undefined;
   const vertex_num = countVertex(seq);
@@ -104,28 +105,55 @@ export function makeAdjMat(seq: Sequence): AdjMat | undefined {
   return mat;
 }
 
-export function calclatePattern(mat?: AdjMat): PatternArray {
+// パターンを列挙したオブジェクト作成
+export function calclatePattern(mat?: AdjMat): Patterns {
   if (mat === undefined) return {};
   const vertex_num = mat.length;
-  let res: PatternArray = {
+  // 0個とすべて埋めたパターン(vertex_num個)は決まっているので前もって入れておく
+  let res: Patterns = {
     0: [new Array(vertex_num).fill(0)],
+    [vertex_num]: [new Array(vertex_num).fill(1)],
   };
-  for (let n = 1; n <= vertex_num; n++) {
+
+  // n = 1の場合を作成する
+  res[1] = [];
+  for (let i = 0; i < vertex_num; i++) {
+    let temp = new Array(vertex_num).fill(0);
+    if (checkOne(mat, i)) {
+      temp[i] = 1;
+      res[1].push(temp);
+    }
+  }
+  // n = 0, 1, 2で完成
+  if (vertex_num === 2) return res;
+
+  // n = 2, ..., (vertex_num-1)の間で調べる
+  for (let n = 2; n <= vertex_num - 1; n++) {
+    console.log("n =", n);
     res[n] = [];
-    // n = 1
-    if (n === 1) {
-      for (let i = 0; i < vertex_num; i++) {
-        let temp = new Array(vertex_num).fill(0);
-        if (checkOne(mat, i)) {
-          temp[i] = 1;
-          res[n].push(temp);
+    // n-1個のパターンを使って計算する
+    for (let prepet of res[n - 1]) {
+      // prepetに頂点を一つ追加して調べる
+      for (let pos = 0; pos < vertex_num; pos++) {
+        if (prepet[pos] === 1) continue;
+        prepet[pos] = 1; // 1を挿入
+        let candidate_pet = Array.from(prepet);
+        if (checkRange(mat, candidate_pet) && !inArray(res[n], candidate_pet)) {
+          res[n].push(candidate_pet);
         }
+        prepet[pos] = 0; // 操作を戻す
       }
     }
-    // n - 1個のパターンを使って探索する。
   }
-
   return res;
+}
+
+function inArray(arrs: number[][], arr: number[]): boolean {
+  if (arrs.length === 0) return false;
+  for (let ar of arrs) {
+    if (ar.toString() === arr.toString()) return true;
+  }
+  return false;
 }
 
 /*
@@ -139,6 +167,9 @@ export function calclatePattern(mat?: AdjMat): PatternArray {
 複数塗る場合一番下（列として見た場合右）の頂点の右に矢印がある場合だめ
 
 複数塗った場合連続していないで離れていればそれぞれのルールで塗る
+NG: false
+OK: true
+とする
 */
 function checkOne(mat: AdjMat, v: number): boolean {
   if (v === 0 && mat[v][v + 1] === 1) return false;
@@ -147,6 +178,55 @@ function checkOne(mat: AdjMat, v: number): boolean {
   return true;
 }
 
-function checkRange(mat: AdjMat, vs: number[]): boolean {
+function checkOneLeft(mat: AdjMat, v: number): boolean {
+  if (v === 0) return true;
+  if (mat[v][v - 1] === 1) return false;
   return true;
+}
+function checkOneRight(mat: AdjMat, v: number): boolean {
+  if (v === mat.length) return true;
+  if (mat[v][v + 1] === 1) return false;
+  return true;
+}
+
+function checkRange(mat: AdjMat, vs: number[]): boolean {
+  console.log("This", vs);
+  let s = vs.indexOf(1);
+  let t = s;
+  while (true) {
+    let temp_t = vs.indexOf(1, t + 1);
+    console.log("TOP", s, t, temp_t);
+    if (temp_t === -1) {
+      console.log("temp_t === -1");
+      if (s !== t) {
+        console.log("D", s, t, temp_t);
+        console.log(checkOneLeft(mat, s), checkOneRight(mat, t));
+        return checkOneLeft(mat, s) && checkOneRight(mat, t);
+      }
+      if (s === t) {
+        console.log("E", s, t, temp_t);
+        return checkOne(mat, s);
+      }
+    }
+    // 中間
+    if (temp_t !== -1) {
+      console.log("temp_t !== -1");
+      if (temp_t === t + 1) {
+        console.log("A", s, t, temp_t);
+        t = temp_t;
+      } else if (temp_t !== t + 1) {
+        console.log("B", s, t, temp_t);
+        console.log(checkOneLeft(mat, s), checkOneRight(mat, t));
+        if (!checkOneLeft(mat, s)) return false;
+        if (!checkOneRight(mat, t)) return false;
+        s = temp_t;
+        t = s;
+      } else {
+        console.log("C", s, t, temp_t);
+        if (!checkOne(mat, s)) return false;
+        s = temp_t;
+        t = s;
+      }
+    }
+  }
 }
